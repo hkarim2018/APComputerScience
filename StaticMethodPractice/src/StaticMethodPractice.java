cfbf15219c14f906fdc3170a436bb6e78f002657
hkarim2018 <hkarim2018@cchsraider.net>
2017-09-21 13:58:31 -0400

    Static Method

---
+++ StaticMethodPractice/src/StaticMethodPractice.java
@@ -0,0 +1,21 @@
+
+public class StaticMethodPractice {
+/*Static Method Practice
+ * Hussein Karim
+ *Prints two boxes
+ */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+box();            //prints first box 
+box();            //prints second box
+	}
+	//Prints a box
+public static void box(){
+	System.out.println("****************" );
+	System.out.println("*              *");
+	System.out.println("*              *");
+	System.out.println("*              *");
+	System.out.println("****************" );
+}
+}
