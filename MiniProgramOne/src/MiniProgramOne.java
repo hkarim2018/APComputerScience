d8b1993e7427b90d799eeee27431023f47355c9f
hkarim2018 <hkarim2018@cchsraider.net>
2017-09-21 13:54:27 -0400

    2nd commit

---
+++ MiniProgramOne/src/MiniProgramOne.java
@@ -0,0 +1,15 @@
+/*Hussein Karim
+ * Program 1
+ * prints victory is mine surrounded in brackets
+ */
+public class MiniProgramOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// prints victory is mine in brackets
+System.out.println("//////////////////////");
+System.out.println("|| Victory is mine! ||");
+System.out.println("//////////////////////");
+	}
+
+}
