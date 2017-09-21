1993e1f7f9f808b2cff2ec298027283264a7bebc
hkarim2018 <hkarim2018@cchsraider.net>
2017-09-21 14:10:09 -0400

    2nd program

---
+++ MiniProgramTwo/src/MiniProgramTwo.java
@@ -0,0 +1,18 @@
+/*Hussein Karim
+ * Program 2
+ * outs the requested text
+ */
+public class MiniProgramTwo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// prints the message
+System.out.println("A well-formed Java program has");
+System.out.println("a main method with { and }");
+System.out.println("braces.");
+System.out.println(); //skips a line
+System.out.println("A System.out.println statement");
+System.out.println("has ( and ) and usually a");
+System.out.println("String enclosed between \" and \"");//escape sequence to write the quotes
+ 
+	}
+}
