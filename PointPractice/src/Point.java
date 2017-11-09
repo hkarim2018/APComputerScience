faf8f7d9aee76eb93aa09732ea249c41ccae715d
hkarim2018 <hkarim2018@cchsraider.net>
2017-11-09 12:30:37 -0500

    first

---
+++ PointPractice/src/Point.java
@@ -0,0 +1,11 @@
+public class Point {
+
+private int x;
+private int y;
+
+public Point(int x,int y){
+	x=0;
+	y=0;
+	}
+
+}
---
+++ PointPractice/src/PointTest.java
@@ -0,0 +1,11 @@
+
+public class PointTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Point test=new Point(4,2);
+
+System.out.println(test);
+	}
+
+}
