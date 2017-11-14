a146d1f96c9bf3a512598d173dd897dd733973c4
darkm_000 <darkm_000@10.0.0.28>
2017-11-13 21:54:25 -0500

    final commit

--- PointPractice/src/Point.java
+++ PointPractice/src/Point.java
@@ -1,11 +1,50 @@
 public class Point {
-
+//declares x and y as private
 private int x;
 private int y;
-
-public Point(int x,int y){
+//constructor for the origin point
+public Point() {
 	x=0;
 	y=0;
+}
+//constructor that creates a new point
+public Point(int newX,int newY){
+	x=newX;
+	y=newY;
 	}
-
+//method that gets a X value 
+public int getX() {
+	return x;
+	}
+//method that gets a Y value
+public int getY() {
+	return y;
+	}
+//method that sets the X value
+public void setX(int newX) {
+	x=newX;
+	}
+//method that sets the Y value
+public void setY(int newY) {
+	y=newY;
+	}
+////method that sets the X and Y values
+public void setLocation(int newX,int newY) {
+	x=newX;
+	y=newY;
+	}
+//translates the point
+public void translate(int dx,int dy) {
+	x+=dx;
+	y+=dy;
+	}
+//gets the distance between two points
+public double distance(Point p) {
+	//creates a x value for a point
+	int x2=p.getX();
+	//creates a y value for a point 
+	int y2=p.getY();
+	//returns the distance
+	return Math.sqrt(Math.pow((x-x2), 2)+Math.pow(y-y2, 2));
+	}
 }
--- PointPractice/src/PointTest.java
+++ PointPractice/src/PointTest.java
@@ -3,9 +3,24 @@ public class PointTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Point test=new Point(4,2);
+Point test=new Point(6,0);//creates point called test
+Point test2=new Point(7,6);// creates point called test2
+Point origin=new Point();//origin point
+System.out.println(origin.getX());//prints the x value of the origin
+System.out.println(origin.getY());//prints the y value of the origin
+test.setX(5);//set a new x value for test
+test.setY(2);//sets a new y value for test
+System.out.println(test.getX());
+System.out.println(test.getY());
+test.setLocation(9, 7);//sets a new location for the x and y values for test
+System.out.println(test.getX());
+System.out.println(test.getY());
+test.translate(1,3);//translates the test point 1 in the x and 3 in the y
+System.out.println(test.getX());
+System.out.println(test.getY());
+//tests the distance between test and test2
+System.out.println(test.distance(test2));
 
-System.out.println(test);
 	}
 
 }
